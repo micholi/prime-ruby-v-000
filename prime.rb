bca36e415f9f0be0f1653822a920d59ff42d91c8
Michelle Olivieri <michelleo72@gmail.com>
2018-03-21 15:43:30 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,7 +5,7 @@ def prime?(integer)
   range_array.split( , )
 
   if integer > 1
-    range.each do |number|
+    range_array.each do |number|
       integer % number == 0
         number != integer
       end

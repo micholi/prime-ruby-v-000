350f512115985aa8c20bc847c17f9e5e14c7c015
Michelle Olivieri <michelleo72@gmail.com>
2018-03-21 15:56:47 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,9 @@ def prime?(integer)
 
   range = (2..integer).to_a
 
-  if integer > 1
+  if integer <= 1
+    false
+  else
     range.each do |number|
       integer % number == 0 && integer != number
 

ee25aedecc3e5580fd37a0d7da89bdb5978dc6f5
Michelle Olivieri <michelleo72@gmail.com>
2018-03-21 15:51:23 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,9 +5,10 @@ def prime?(integer)
 
   if integer > 1
     range.each do |number|
-      integer % number == 0
-        number != integer
+      integer % number == 0 && number != integer
         true
+      else
+        false
       end
   end
 end
